188bfc558a98fca2f3c14ed08ce31eb88f32162c
pasquini777 <santoskayk950@gmail.com>
2025-10-24 00:36:50 +0000

    loops aninhados

--- xadrez.c
+++ xadrez.c
@@ -17,8 +17,10 @@ int main() {
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
- char pecas[] = {'torre', 'bispo', 'rainha'};
+ char pecas[] = {'torre', 'bispo', 'rainha', 'cavalo'};
     int i;
+    int movimentocompleto = 1;
+
 
     // Mostrar movimentos da torre 5 casas para a direita
     printf("torre :\n");
@@ -26,7 +28,7 @@ int main() {
     {
         printf("Direita\n"); // impreme a direção do movimento
     }
-
+    printf("\n");
     // Mostrar movimentos do bispo 5 casas na diagonal superior direita
     printf("bispo :\n");
     i = 0;
@@ -35,7 +37,9 @@ int main() {
         printf("Cima, direita\n"); // imprime a direção do movimento
         i++;
     }
-    
+
+    printf("\n");
+
     // Mostrar movimentos da rainha 8 casas para a esquerda
     printf("rainha :\n");
     i = 0;
@@ -45,11 +49,25 @@ int main() {
         i++;
     } while (i < 8);
        
-    
+    printf("\n");
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+    printf("cavalo :\n");
+    while (movimentocompleto--)
+    {
+
+        for(int i = 0; i < 2; i++){
+            printf("baixo\n");
+        }
+        printf("esquerda\n");
+    }
+    
+
+
+
+
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
